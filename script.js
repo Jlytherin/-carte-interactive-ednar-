fc180aaa10a1575226d8b0a01f5969803d47d60e
Jlytherin <ednaramavi@gmail.com>
2020-04-12 20:06:45 +0200

    Modification html

--- script.js
+++ script.js
@@ -18,3 +18,52 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     zoomOffset: -1,
     accessToken: 'your.mapbox.access.token'
 }).addTo(mymap);
+
+
+async function getData() {
+	let url = "https://opendata.paris.fr/api/records/1.0/search/?dataset=que-faire-a-paris-&q=date_start+%3E%3D+%23now()+AND+date_start+%3C+%23now(months%3D1)&rows=50&facet=category&facet=tags&facet=address_zipcode&facet=address_city&facet=pmr&facet=blind&facet=deaf&facet=access_type&facet=price_type"
+	let response = await fetch(url);
+    
+  let data = await response.json();
+
+  data.records.forEach(function(event) {
+		// le titre de l'événement
+    let title = event.fields.title;
+
+		// si jamais le champs latitude/longitude manque
+		// on ignore cet événement
+		if (!event.fields.lat_lon) {
+			return;
+    }
+
+		// la latitude
+    let latitude = event.fields.lat_lon[0];
+    
+    let addresse_name = event.addresse_name;
+
+		// la longitude
+	  let longitude = event.fields.lat_lon[1];
+		// on pourrait récupérer d'autres infos..
+
+		// pour tester, on les affiche dans la console
+    console.log(title + " " + latitude + " " + longitude);
+    
+    var marker = L.marker([latitude, longitude]);
+    // .. mais ce serait mieux de les afficher sur la carte !
+    
+
+    // afficher un popup 
+    let popupContent = "Event " + title +  "<br>" + addresse_name; 
+    marker.bindPopup(popupContent);
+
+    marker.addTo(mymap);
+    })
+}
+
+getData()
+
+function OnFormSubmit(event){
+event.preventDefault();
+console.log("Le formulaire à bien été envoyé");
+console.log(searchInput.value);
+}
